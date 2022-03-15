67d48001fe5bbfd7fcfae3a3ed4c2484afc633f3
Piotr <piotr.zywczak@microsoft.wsei.edu.pl>
2022-03-15 16:36:27 +0100

    Update StartingUserInterfaces's HelpCommand method  Add IsWeighted property to BaseGradeBook.  Refactor constructor of BaseGradeBook Note

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    class StandardGradeBook : BaseGradeBook
+        public class StandardGradeBook : BaseGradeBook
     {
-        public StandardGradeBook(string name, bool enable) : base(name)
+        public StandardGradeBook(string name, bool isWeighted) : base(name, isWeighted)
         {
             Type = Enums.GradeBookType.Standard;
         }
